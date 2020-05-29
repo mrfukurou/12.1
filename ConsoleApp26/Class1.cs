1d886100df1e9b9b4d376fb9a570209183f8f29b
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-29 23:06:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp26/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp26
+{
+    class mas
+    {
+        int[,] IntArray;
+        int n,m;
+
+        public mas() { }
+
+        public mas(int N, int M)
+        {
+            n = N;
+            m = M;
+            IntArray = new int[n, m];
+        }
+
+        public int[,] Input()
+        {
+            Console.WriteLine("Введите элементы массива: ");
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    IntArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            return IntArray;
+
+        }
+
+        public void Output()
+        {
+            for (int i = 0; i < IntArray.GetLength(0); ++i, Console.WriteLine())
+                for (int j = 0; j < IntArray.GetLength(1); ++j)
+                    Console.Write("{0,5} ", IntArray[i, j]);
+        }
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 || i > IntArray.GetLength(0) - 1 && j < 0 || j > IntArray.GetLength(1) - 1)
+                {
+                    throw new Exception("Элемента с таким индексом нет.");
+                }
+                return IntArray[i, j];
+            }
+            set
+            {
+                if (i < 0 || i > IntArray.GetLength(0) - 1 && j < 0 || j > IntArray.GetLength(1) - 1)
+                {
+                    throw new Exception("Элемента с таким индексом нет.");
+                }
+                IntArray[i, j] = value;
+            }
+        }
+       
+        public static mas operator ++(mas increment)
+        {
+            for (int i = 0; i < increment.IntArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < increment.IntArray.GetLength(1); j++)
+                    increment.IntArray[i, j] += 1;
+            }
+            return increment;
+        }
+        public static mas operator --(mas decrement)
+        {
+            for (int i = 0; i < decrement.IntArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < decrement.IntArray.GetLength(1); j++)
+                    decrement.IntArray[i, j] -= 1;
+            }
+            return decrement;
+        }
+
+        public static bool operator true(mas q)
+        {
+            int n = q.IntArray.GetLength(0);
+            int m = q.IntArray.GetLength(1);
+            if (n == m) return true;
+            else return false;
+        }
+        public static bool operator false(mas q)
+        {
+            int n = q.IntArray.GetLength(0);
+            int m = q.IntArray.GetLength(1);
+            if (n != m) return false;
+            else return true;
+        }
+
+        public static mas operator +(mas a, mas b)
+        {
+                int[,] intArray = new int[a.IntArray.GetLength(0), b.IntArray.GetLength(1)];
+                mas result = new mas();
+                for (int i = 0; i < a.IntArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.IntArray.GetLength(1); j++)
+                    {
+                        intArray[i, j] = a[i, j] + b[i, j];
+                    }
+                }
+                result.IntArray = intArray;
+            return result;
+        }
+        public static explicit operator int[,](mas p)
+        {
+            return p.IntArray;
+        }
+
+        public static explicit operator mas(int[,] IntArray)
+        {
+            mas p = new mas(IntArray.GetLength(0), IntArray.GetLength(1));
+            IntArray.CopyTo(p.IntArray, 0);
+            return p;
+        }
+    }
+}
+
+
---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите количество строк: ");               
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите количество столбцов: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                if (n > 1 && m > 1)
+                {
+                    mas ms = new mas(n,m);
+                    int[,] IntArray = ms.Input();
+                    Console.WriteLine("Матрица: ");
+                    ms.Output();
+                    Console.WriteLine("Перегрузка ++: ");
+                    ms++;
+                    ms.Output();
+                    Console.WriteLine("Перегрузка --: ");
+                    ms--;
+                    ms.Output();
+                    Console.Write("Введите номер номер строки для обращения к элементу: ");
+                    int i = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите номер номер столбца для обращения к элементу: ");
+                    int j = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(ms[i, j]);
+                    Console.WriteLine();
+                    Console.WriteLine("Перегрузка констант true/false\nМатрица является квадратной:");
+                    if (ms)
+                        Console.WriteLine(true);
+                    else
+                        Console.WriteLine(false);
+                    Console.WriteLine();
+                    Console.Write("Введите количество строк: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите количество столбцов: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    if (a == n && b == m)
+                    {
+                        mas ms1 = new mas(a, b);
+                        int[,] IntArray1 = ms1.Input();
+                        Console.WriteLine("Матрица: ");
+                        ms1.Output();
+
+                        mas ms2 = ms + ms1;
+                        Console.WriteLine("Сумма двух матриц: ");
+                        ms2.Output();
+                    }
+                    else { throw new Exception("Матрицы имеют разную размерность, нельзя сложить!"); }
+                    int[,] s = (int[,])ms;
+                    Console.WriteLine(s);
+                    mas p = (mas)ms;
+                    Console.WriteLine(p);
+                }
+                else { throw new Exception("Число должно быть больше единицы!"); }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Некорректный ввод! {ex.Message}");
+            }
+
+        }
+    }
+}
